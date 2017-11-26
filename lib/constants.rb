5abd752bf164d141d7931584061784e30e00138d
Jason Yoo <jasonyoo0116@gmail.com>
2017-11-26 13:19:10 -0800

    Finished on Nov.26 production

--- lib/constants.rb
+++ lib/constants.rb
@@ -2,7 +2,7 @@ module Constants
 	# Have SCREEN_GAP, paths to the program
 	SCREEN_GAP = 64
 	SCREEN_SIZE = 80
-	DURATION_MODIFIER = 1 # Should be 60 for production
+	DURATION_MODIFIER = 60 # Should be 60 for production
 
 	APP_ROOT = File.expand_path('.')
 	LIB_PATH = File.join(APP_ROOT, 'lib')
